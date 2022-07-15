abb1a202ea8b09a0244cc4238aadbb899d1d41d5
theodore brockman <tmanbrock@gmail.com>
2022-07-15 12:27:32 -0700

    Stops pruning already grouped tabs. Add tests.

--- src/handlers/alarm.ts
+++ src/handlers/alarm.ts
@@ -1,62 +1,87 @@
-import { TabGrouper, TabPruner, TabTracker } from "../tabs"
-import { Tab } from "../types"
+import { TabGrouper, TabPruner, TabTracker } from '../tabs';
+import { Tab } from '../types';
 
-const toOneDayInMilliseconds = 24 * 60 * 60 * 1000
+const toOneDayInMilliseconds = 24 * 60 * 60 * 1000;
 
 type AlarmHandlerArgs = {
-    tracker: TabTracker
-    grouper: TabGrouper
-    pruner: TabPruner
-    options: any
-}
+	tracker: TabTracker;
+	grouper: TabGrouper;
+	pruner: TabPruner;
+	options: any;
+};
 
 class AlarmHandler {
-    tracker: TabTracker
-    grouper: TabGrouper
-    pruner: TabPruner
-    autoPrune: boolean
-    pruneThreshold: number
-    autoGroup: boolean
-    autoGroupThreshold: number
-    autoGroupName: string
-    autoBookmark: boolean
+	tracker: TabTracker;
+	grouper: TabGrouper;
+	pruner: TabPruner;
+	autoPrune: boolean;
+	pruneThreshold: number;
+	autoGroup: boolean;
+	autoGroupThreshold: number;
+	autoGroupName: string;
+	autoBookmark: boolean;
 
-    constructor({tracker, grouper, pruner, options}: AlarmHandlerArgs) {
-        this.tracker = tracker
-        this.grouper = grouper
-        this.pruner = pruner
-        this.autoPrune = options['auto-prune']
-        this.pruneThreshold = options['prune-threshold'] * toOneDayInMilliseconds
-        this.autoGroup = options['auto-group']
-        this.autoGroupThreshold = options['auto-group-threshold'] * toOneDayInMilliseconds
-        this.autoGroupName = options['auto-group-name']
-        this.autoBookmark = options['auto-prune-bookmark']
-    }
+	constructor({ tracker, grouper, pruner, options }: AlarmHandlerArgs) {
+		this.tracker = tracker;
+		this.grouper = grouper;
+		this.pruner = pruner;
+		this.autoPrune = options['auto-prune'];
+		this.pruneThreshold =
+			options['prune-threshold'] * toOneDayInMilliseconds;
+		this.autoGroup = options['auto-group'];
+		this.autoGroupThreshold =
+			options['auto-group-threshold'] * toOneDayInMilliseconds;
+		this.autoGroupName = options['auto-group-name'];
+		this.autoBookmark = options['auto-prune-bookmark'];
+	}
 
-    async execute() {
-        let openTabs = await chrome.tabs.query({})
-        let candidates: Tab[] = []
-        await this.tracker.init(openTabs)
-        console.debug('open tabs', openTabs)
-    
-        if (this.autoPrune) {
-            [candidates, openTabs] = this.tracker.findTabsExceedingThreshold(openTabs, this.pruneThreshold)
-            console.debug('should be pruning', candidates)
-            await this.pruner.pruneTabs(candidates)
-        }
-        console.debug('remaining tabs', openTabs)
-    
-        if (this.autoGroup) {
-            const group = {
-                title: this.autoGroupName,
-                color: "yellow",
-                collapsed: true
-            }
-            const [toGroup, ] = this.tracker.findTabsExceedingThreshold(openTabs, this.autoGroupThreshold)
-            console.debug('should be grouping', toGroup)
-            await this.grouper.groupTabs(toGroup, group)
-        }
-    }
+	async execute() {
+		let openTabs = await chrome.tabs.query({});
+		let candidates: Tab[] = [];
+		await this.tracker.init(openTabs);
+		console.debug('open tabs', openTabs);
+		const group = {
+			title: this.autoGroupName,
+			color: 'yellow',
+			collapsed: true,
+		};
+
+		if (this.autoPrune) {
+			[candidates, openTabs] = this.tracker.findTabsExceedingThreshold(
+				openTabs,
+				this.pruneThreshold,
+			);
+			const filter = [-1];
+
+			if (this.autoGroup) {
+				const groups = await chrome.tabGroups.query({
+					title: group['title'],
+				});
+
+				if (groups.length > 0) {
+					filter.push(groups[0].id);
+				}
+			}
+			console.debug('before filtering any grouped tabs', candidates);
+			// leave any tabs which have a groupId
+			// that isn't our autoGroup alone
+			candidates = candidates.filter((tab) =>
+				filter.includes(tab.groupId ?? -1),
+			);
+			console.debug('should be pruning', candidates);
+			await this.pruner.pruneTabs(candidates);
+		}
+		console.debug('remaining tabs', openTabs);
+
+		if (this.autoGroup) {
+			const [toGroup] = this.tracker.findTabsExceedingThreshold(
+				openTabs,
+				this.autoGroupThreshold,
+			);
+			console.debug('should be grouping', toGroup);
+			await this.grouper.groupTabs(toGroup, group);
+		}
+	}
 }
 
-export default AlarmHandler
+export default AlarmHandler;
--- tests/handlers/alarm.test.js
+++ tests/handlers/alarm.test.js
@@ -1,48 +1,142 @@
 // TODO
 
-import { AlarmHandler } from '../../src/handlers/alarm'
-import { assert } from 'chai'
-import { TabBookmarker, TabGrouper, TabPruner, TabTracker } from '../../src/tabs';
-
+import AlarmHandler from '../../src/handlers/alarm';
+import { TabGrouper, TabPruner, TabTracker } from '../../src/tabs';
+import sinon from 'sinon/pkg/sinon-esm';
 const chrome = require('sinon-chrome/extensions');
 
 describe('alarm-handler', () => {
+	let handler;
+	let tracker;
+	let grouper;
+	let pruner;
 
-    let handler
-    let tracker
-    let grouper
-    let pruner
-    let bookmarker
+	before(() => {
+		chrome.tabs.group = sinon.stub();
+		chrome.tabGroups = {
+			query: sinon.stub(),
+			update: sinon.stub(),
+			move: sinon.stub(),
+		};
+		global.chrome = chrome;
+		global.chrome.flush();
+	});
 
-    before(() => {
-        global.chrome = chrome
-        global.chrome.flush()
-    })
+	const createOptions = (overrides) => {
+		const options = {
+			'auto-deduplicate': true,
+			'auto-prune': true,
+			'prune-threshold': 7,
+			'auto-group': true,
+			'auto-group-threshold': 3,
+			'auto-group-name': '🕒 old tabs',
+			'auto-prune-bookmark': false,
+			'auto-prune-bookmark-name': '🌱 pruned',
+			'tab-lru-enabled': false,
+			'tab-lru-size': 30,
+			'tab-lru-destination': 'group',
+			'show-hints': true,
+		};
+		return { ...options, ...overrides };
+	};
 
-    const createAlarmHandler = (options) => {
-        tracker = new TabTracker()
-        grouper = new TabGrouper()
-    
-        if (options['auto-prune-bookmark']) {
-            bookmarker = new TabBookmarker(options['auto-prune-bookmark-name'])
-        }
-        pruner = new TabPruner(bookmarker)
-        handler = new AlarmHandler({ tracker, grouper, pruner, options })
-    }
+	const createAlarmHandler = (options) => {
+		tracker = sinon.createStubInstance(TabTracker);
+		grouper = sinon.createStubInstance(TabGrouper);
+		pruner = sinon.createStubInstance(TabPruner);
+		return new AlarmHandler({ tracker, grouper, pruner, options });
+	};
 
-    afterEach(() => {
-        global.chrome.flush()
-    })
+	afterEach(() => {
+		global.chrome.flush();
+	});
 
-    it('shouldnt remove or group any tabs', async() => {
+	it('shouldnt remove or group any tabs', async () => {
+		const options = createOptions({});
+		handler = createAlarmHandler(options);
+		const tabs = [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: 1 },
+			{ id: 3, groupId: 2 },
+		];
+		const groups = [{ id: 1 }];
+		tracker.findTabsExceedingThreshold.returns([[], tabs]);
+		chrome.tabs.query.resolves(tabs);
+		chrome.tabGroups.query.resolves(groups);
+		await handler.execute();
+		sinon.assert.calledWith(pruner.pruneTabs, []);
+	});
 
-    })
+	it('should remove but not group tabs', async () => {
+		const options = createOptions({});
+		handler = createAlarmHandler(options);
+		const tabs = [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: -1 },
+			{ id: 3, groupId: -1 },
+		];
+		const groups = [{ id: 1 }];
+		tracker.findTabsExceedingThreshold
+			.onCall(0)
+			.returns([tabs.slice(0, 1), [tabs[2]]]);
+		tracker.findTabsExceedingThreshold.onCall(1).returns([[], [tabs[2]]]);
+		chrome.tabs.query.resolves(tabs);
+		chrome.tabGroups.query.resolves(groups);
+		await handler.execute();
+		sinon.assert.calledWith(pruner.pruneTabs, tabs.slice(0, 1));
+		sinon.assert.calledWith(grouper.groupTabs, []);
+	});
 
-    it('should remove but not group tabs', async() => {
+	it('should group but not remove tabs', async () => {
+		const options = createOptions({});
+		handler = createAlarmHandler(options);
+		const tabs = [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: -1 },
+			{ id: 3, groupId: -1 },
+		];
+		const groups = [{ id: 1 }];
+		tracker.findTabsExceedingThreshold.onCall(0).returns([[], tabs]);
+		tracker.findTabsExceedingThreshold.onCall(1).returns([tabs]);
+		chrome.tabs.query.resolves(tabs);
+		chrome.tabGroups.query.resolves(groups);
+		await handler.execute();
+		sinon.assert.calledWith(pruner.pruneTabs, []);
+		sinon.assert.calledWith(grouper.groupTabs, tabs);
+	});
 
-    })
+	it('should not remove any user grouped tabs', async () => {
+		const options = createOptions({});
+		handler = createAlarmHandler(options);
+		const tabs = [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: 1 },
+			{ id: 3, groupId: 2 },
+		];
+		const groups = [{ id: 1 }];
+		tracker.findTabsExceedingThreshold.returns([tabs, []]);
+		chrome.tabs.query.resolves(tabs);
+		chrome.tabGroups.query.resolves(groups);
+		await handler.execute();
+		sinon.assert.calledWith(pruner.pruneTabs, [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: 1 },
+		]);
+	});
 
-    it('should group but not remove tabs', async() => {
-
-    })
-})
+	it('should not remove any user grouped tabs (no existing tab group)', async () => {
+		const options = createOptions({});
+		handler = createAlarmHandler(options);
+		const tabs = [
+			{ id: 1, groupId: -1 },
+			{ id: 2, groupId: 1 },
+			{ id: 3, groupId: 2 },
+		];
+		const groups = [];
+		tracker.findTabsExceedingThreshold.returns([tabs, []]);
+		chrome.tabs.query.resolves(tabs);
+		chrome.tabGroups.query.resolves(groups);
+		await handler.execute();
+		sinon.assert.calledWith(pruner.pruneTabs, [{ id: 1, groupId: -1 }]);
+	});
+});
