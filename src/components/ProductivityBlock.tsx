6e15b6e571aaa11d77d9827fe066979819113716
theodore brockman <tmanbrock@gmail.com>
2023-08-20 11:56:35 -0400

    Update productivity mode hint. Update publish to only publish Chrome since others not yet available.

--- src/components/ProductivityBlock.tsx
+++ src/components/ProductivityBlock.tsx
@@ -1,63 +1,60 @@
-import React from "react";
-import { FormControlLabel, IconButton, Tooltip } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { FormOption } from "./FormOption";
-import LabelWithHint from "./LabelWithHint";
-import PersistedInput from "./PersistedInput";
-import { useStore, Page } from "../hooks/useStore";
-import { StorageKeys } from "~enums";
+import React from 'react';
+import { FormControlLabel, IconButton, Tooltip } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { FormOption } from './FormOption';
+import LabelWithHint from './LabelWithHint';
+import PersistedInput from './PersistedInput';
+import { useStore, Page } from '../hooks/useStore';
+import { StorageKeys } from '~enums';
 
 export default function ProductivityBlock() {
-  const setPage = useStore((state) => state.setPage);
-  const page = useStore((state) => state.page);
+	const setPage = useStore((state) => state.setPage);
+	const page = useStore((state) => state.page);
 
-  const hint =
-    "helps keep your browsing productive by blocking use of typical time wasting websites.";
-  const label = "turn on productivity mode 👨‍💻";
+	const hint =
+		'helps keep your browsing productive by blocking use of typical time wasting websites.';
+	const label = 'turn on productivity mode 👨‍💻';
+	const settingsHint = 'change which websites to block.';
 
-  const settingsIconClicked = () => {
-    setPage(Page.ProductivitySettings);
-  };
+	const settingsIconClicked = () => {
+		setPage(Page.ProductivitySettings);
+	};
 
-  const backButtonClicked = () => {
-    setPage(Page.Home);
-  };
+	const backButtonClicked = () => {
+		setPage(Page.Home);
+	};
 
-  return (
-    <FormOption>
-      <FormControlLabel
-        control={
-          <PersistedInput
-            component="checkbox"
-            storageKey={StorageKeys.PRODUCTIVITY_MODE_ENABLED}
-          />
-        }
-        label={
-          <LabelWithHint
-            hint={hint}
-            label={label}
-            tooltipProps={{ placement: "top" }}
-          />
-        }
-      />
-      {page !== Page.ProductivitySettings ? (
-        <Tooltip
-          placement="top"
-          arrow={true}
-          title="change productivity mode settings"
-        >
-          <IconButton aria-label="settings" onClick={settingsIconClicked}>
-            <SettingsIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip placement="top" arrow={true} title="back to main options">
-          <IconButton aria-label="main options" onClick={backButtonClicked}>
-            <ArrowBackIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </FormOption>
-  );
+	return (
+		<FormOption>
+			<FormControlLabel
+				control={
+					<PersistedInput
+						component="checkbox"
+						storageKey={StorageKeys.PRODUCTIVITY_MODE_ENABLED}
+					/>
+				}
+				label={
+					<LabelWithHint
+						hint={hint}
+						label={label}
+						tooltipProps={{ placement: 'top' }}
+					/>
+				}
+			/>
+			{page !== Page.ProductivitySettings ? (
+				<Tooltip placement="top" arrow={true} title={settingsHint}>
+					<IconButton aria-label="settings" onClick={settingsIconClicked}>
+						<SettingsIcon />
+					</IconButton>
+				</Tooltip>
+			) : (
+				<Tooltip placement="top" arrow={true} title="back to main options">
+					<IconButton aria-label="main options" onClick={backButtonClicked}>
+						<ArrowBackIcon />
+					</IconButton>
+				</Tooltip>
+			)}
+		</FormOption>
+	);
 }
